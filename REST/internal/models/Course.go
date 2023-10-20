ed1279de749c8aab5308c685256e6cf9863fb68a
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-10-20 22:17:58 +0700

    move to auth service

--- REST/internal/models/Course.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-type Course struct {
-	gorm.Model
-	CourseName string `gorm:"type:varchar(255);notNull" json:"course_name"`
-	CourseCode string `gorm:"type:varchar(255);uniqueIndex;notNull" json:"course_code"`
-}
