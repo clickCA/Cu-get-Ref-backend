105612c7360fc2dc783e8db9e7301953d178e57e
Nick <nick.rattanaton@gmail.com>
2023-10-24 22:09:58 +0700

    ➗ Initial with static value

--- course-management-service/main.go
+++ course-management-service/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	course_management_service "course-management-service/coursemanagement"
+	course_management "course-management-service/coursemanagement"
 	"log"
 	"net"
 
@@ -10,21 +10,33 @@ import (
 )
 
 type server struct {
-	course_management_service.UnimplementedCourseManagementServiceServer
+	course_management.UnimplementedCourseManagementServiceServer
 }
 
-func (s *server) GetAllSubject(context.Context, *course_management_service.Empty) (*course_management_service.SubjectList, error) {
-	return &course_management_service.SubjectList{
-		Subject: []*course_management_service.SubjectItem{
+func (s *server) GetAllSubject(context.Context, *course_management.Empty) (*course_management.SubjectList, error) {
+	return &course_management.SubjectList{
+		Subject: []*course_management.SubjectItem{
 			{
-				Id: "1",
-				Name: "Math",
-				Price: 3,
-			},
-			{
-				Id: "2",
-				Name: "English",
-				Price: 2,
+				SubjectId: "2110415",
+				SubjectName: "Software Architecture",
+				CourseDescription: "1. Introduction to Software Architecture",
+				FacultyDepartment: "Computer Engineering",
+				AcademicTerm: "1",
+				AcademicYear: 2563,
+				Professors: []*course_management.Professor{
+					{
+						ProfessorName: "Dr. Somchai",
+					},
+				},
+				Prerequisites: []*course_management.Prerequisite{
+					{
+						SubjectId: "2110211",
+					},
+				},
+				Status: "open",
+				CurriculumName: "Computer Engineering",
+				DegreeLevel: "bachelor",
+				TeachingHours: 3,
 			},
 		},
 		}, nil
@@ -39,9 +51,10 @@ func main() {
 	s := grpc.NewServer()
 
 	// Attach the CourseManagement service to the server
-	course_management_service.RegisterCourseManagementServiceServer(s, &server{})
+	course_management.RegisterCourseManagementServiceServer(s, &server{})
 
 	// Start the server
+	log.Println("Server is listening on port 50051...")
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
