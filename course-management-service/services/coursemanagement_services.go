3430ba09985b41728397fcf10618ecfe1e60fa81
Nick <nick.rattanaton@gmail.com>
2023-10-24 23:48:13 +0700

    ➕ Add all method

--- course-management-service/services/coursemanagement_services.go
+++ course-management-service/services/coursemanagement_services.go
@@ -13,7 +13,7 @@ type Server struct {
 	course_management.UnimplementedCourseManagementServiceServer
 }
 
-var dsn = "root:password@localhost:3307/db?charset=utf8mb4&parseTime=True&loc=Local"
+var dsn = "root:password@tcp(localhost:3307)/db?charset=utf8mb4&parseTime=True"
 var db, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 type Subject struct {
@@ -23,8 +23,8 @@ type Subject struct {
 	FacultyDepartment string
 	AcademicTerm string
 	AcademicYear int32
-	Professors []*string
-	Prerequisites []*string
+	Professors string
+	Prerequisites string
 	Status string
 	CurriculumName string
 	DegreeLevel string
@@ -39,65 +39,47 @@ type Subject struct {
 // 	DeleteSubject(ctx context.Context, in *SubjectId, opts ...grpc.CallOption) (*Empty, error)
 // }
 func (s *Server) GetAllSubject(ctx context.Context,req *course_management.Empty) (*course_management.SubjectList, error) {
-	log.Println("GetAllSubject")
-	return &course_management.SubjectList{
-		Subject: []*course_management.SubjectItem{
-			{
-				SubjectId: "2110415",
-				SubjectName: "Software Architecture",
-				CourseDescription: "1. Introduction to Software Architecture",
-				FacultyDepartment: "Computer Engineering",
-				AcademicTerm: "1",
-				AcademicYear: 2563,
-				Professors: []*course_management.Professor{
-					{
-						ProfessorName: "Dr. Somchai",
-					},
-				},
-				Prerequisites: []*course_management.Prerequisite{
-					{
-						SubjectId: "2110211",
-					},
-				},
-				Status: "open",
-				CurriculumName: "Computer Engineering",
-				DegreeLevel: "bachelor",
-				TeachingHours: 3,
-			},
-		},
-		}, nil
+	log.Println("[Maintenance] GetAllSubject")
+	return &course_management.SubjectList{}, nil
 }
 
 func (s *Server) GetSubject(ctx context.Context,req *course_management.SubjectId) (*course_management.SubjectItem, error) {
-	log.Println("GetSubject")
-	log.Println("Getting ",req.GetSubjectId())
+	log.Println("GetSubject", req.GetSubjectId())
+	var subject Subject
+	db.First(&subject, "subject_id = ?", req.GetSubjectId())
 	return &course_management.SubjectItem{
-		SubjectId: "2110415",
-		SubjectName: "Software Architecture",
-		CourseDescription: "1. Introduction to Software Architecture",
-		FacultyDepartment: "Computer Engineering",
-		AcademicTerm: "1",
-		AcademicYear: 2563,
-		Professors: []*course_management.Professor{
-			{
-				ProfessorName: "Dr. Somchai",
-			},
-		},
-		Prerequisites: []*course_management.Prerequisite{
-			{
-				SubjectId: "2110211",
-			},
-		},
-		Status: "open",
-		CurriculumName: "Computer Engineering",
-		DegreeLevel: "bachelor",
-		TeachingHours: 3,
+		SubjectId: subject.SubjectId,
+		SubjectName: subject.SubjectName,
+		CourseDescription: subject.CourseDescription,
+		FacultyDepartment: subject.FacultyDepartment,
+		AcademicTerm: subject.AcademicTerm,
+		AcademicYear: subject.AcademicYear,
+		Professors: []*course_management.Professor{},
+		Prerequisites: []*course_management.Prerequisite{},
+		Status: subject.Status,
+		CurriculumName: subject.CurriculumName,
+		DegreeLevel: subject.DegreeLevel,
+		TeachingHours: subject.TeachingHours,
 	}, nil
 }
 
 func (s *Server) AddNewSubject(ctx context.Context,req *course_management.SubjectItem) (*course_management.SubjectItem, error) {
-	log.Println("AddNewSubject")
-	log.Println("Adding ",req.String())
+	log.Println("AddNewSubject", req.GetSubjectId())
+	subject := Subject{
+		SubjectId: req.GetSubjectId(),
+		SubjectName: req.GetSubjectName(),
+		CourseDescription: req.GetCourseDescription(),
+		FacultyDepartment: req.GetFacultyDepartment(),
+		AcademicTerm: req.GetAcademicTerm(),
+		AcademicYear: req.GetAcademicYear(),
+		Professors: "test",
+		Prerequisites: "test",
+		Status: req.GetStatus(),
+		CurriculumName: req.GetCurriculumName(),
+		DegreeLevel: req.GetDegreeLevel(),
+		TeachingHours: req.GetTeachingHours(),
+	}
+	db.Create(&subject)
 	return &course_management.SubjectItem{
 		SubjectId: req.GetSubjectId(),
 		SubjectName: req.GetSubjectName(),
@@ -117,6 +99,21 @@ func (s *Server) AddNewSubject(ctx context.Context,req *course_management.Subjec
 func (s *Server) UpdateSubjectDetail(ctx context.Context,req *course_management.SubjectItem) (*course_management.SubjectItem, error) {
 	log.Println("UpdateSubjectDetail")
 	log.Println("Updating ",req.String())
+	subject := Subject{
+		SubjectId: req.GetSubjectId(),
+		SubjectName: req.GetSubjectName(),
+		CourseDescription: req.GetCourseDescription(),
+		FacultyDepartment: req.GetFacultyDepartment(),
+		AcademicTerm: req.GetAcademicTerm(),
+		AcademicYear: req.GetAcademicYear(),
+		Professors: "test",
+		Prerequisites: "test",
+		Status: req.GetStatus(),
+		CurriculumName: req.GetCurriculumName(),
+		DegreeLevel: req.GetDegreeLevel(),
+		TeachingHours: req.GetTeachingHours(),
+	}
+	db.Save(&subject)
 	return &course_management.SubjectItem{
 		SubjectId: req.GetSubjectId(),
 		SubjectName: req.GetSubjectName(),
@@ -136,5 +133,6 @@ func (s *Server) UpdateSubjectDetail(ctx context.Context,req *course_management.
 func (s *Server) DeleteSubject(ctx context.Context,req *course_management.SubjectId) (*course_management.Empty, error) {
 	log.Println("DeleteSubject")
 	log.Println("Deleting ",req.GetSubjectId())
+	db.Delete(&Subject{}, req.GetSubjectId())
 	return &course_management.Empty{}, nil
 }
