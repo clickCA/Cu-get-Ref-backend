d3c56c8d4854cafd1a7fffb48a5ded508fce90e1
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-10-24 19:51:33 +0700

    add model

---
+++ authservice/models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+// User struct
+type User struct {
+	gorm.Model
+	Email        string `gorm:"uniqueIndex"`
+	PasswordHash string
+	Role         Role
+}
