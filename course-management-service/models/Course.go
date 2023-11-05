3ceaeae00df784d053ee9df8d62e69862b29b233
Nick <nick.rattanaton@gmail.com>
2023-11-05 12:01:16 +0700

    ➗ Update service to have more readable

--- course-management-service/models/Course.go
+++ course-management-service/models/Course.go
@@ -14,3 +14,20 @@ type Course struct {
 	DegreeLevel       string
 	TeachingHours     int32
 }
+
+func NewCourse(courseId string, courseName string, courseDescription string, facultyDepartment string, academicTerm string, academicYear int32, professors string, prerequisites string, status string, curriculumName string, degreeLevel string, teachingHours int32) *Course {
+	course := new(Course)
+	course.CourseId = courseId
+	course.CourseName = courseName
+	course.CourseDescription = courseDescription
+	course.FacultyDepartment = facultyDepartment
+	course.AcademicTerm = academicTerm
+	course.AcademicYear = academicYear
+	course.Professors = professors
+	course.Prerequisites = prerequisites
+	course.Status = status
+	course.CurriculumName = curriculumName
+	course.DegreeLevel = degreeLevel
+	course.TeachingHours = teachingHours
+	return course
+}
--- course-management-service/services/coursemanagement_services.go
+++ course-management-service/services/coursemanagement_services.go
@@ -14,101 +14,134 @@ type Server struct {
 
 var db = config.ConnectDB()
 
-func (s *Server) GetAllSubject(ctx context.Context, req *course_management.Empty) (*course_management.CourseList, error) {
-	log.Println("[Maintenance] GetAllSubject")
-	return &course_management.CourseList{}, nil
+func (s *Server) GetAllCourses(ctx context.Context, req *course_management.Empty) (*course_management.CourseList, error) {
+	log.Println("GetAllCourses")
+	// Get all course in database
+	var courses []models.Course
+	db.Find(&courses)
+	// Convert to protobuf
+	var courseList []*course_management.CourseItem
+	for _, subject := range courses {
+		courseList = append(courseList, &course_management.CourseItem{
+			CourseId:          subject.CourseId,
+			CourseName:        subject.CourseName,
+			CourseDescription: subject.CourseDescription,
+			FacultyDepartment: subject.FacultyDepartment,
+			AcademicTerm:      subject.AcademicTerm,
+			AcademicYear:      subject.AcademicYear,
+			Professors:        []*course_management.Professor{},
+			Prerequisites:     []*course_management.Prerequisite{},
+			Status:            subject.Status,
+			CurriculumName:    subject.CurriculumName,
+			DegreeLevel:       subject.DegreeLevel,
+			TeachingHours:     subject.TeachingHours,
+		})
+	}
+	// Return
+	return &course_management.CourseList{
+		Course: courseList,
+	}, nil
 }
 
-func (s *Server) GetSubject(ctx context.Context, req *course_management.CourseId) (*course_management.CourseItem, error) {
-	log.Println("GetSubject", req.GetCourseId())
-	var subject models.Course
-	db.First(&subject, "subject_id = ?", req.GetCourseId())
-	return &course_management.CourseItem{
-		CourseId:          subject.CourseId,
-		CourseName:        subject.CourseName,
-		CourseDescription: subject.CourseDescription,
-		FacultyDepartment: subject.FacultyDepartment,
-		AcademicTerm:      subject.AcademicTerm,
-		AcademicYear:      subject.AcademicYear,
+func (s *Server) GetCourse(ctx context.Context, req *course_management.CourseId) (*course_management.CourseItem, error) {
+	log.Println("GetCourse", req.GetCourseId())
+	// Get course in database
+	var course models.Course
+	db.First(&course, req.GetCourseId())
+	// Convert to protobuf
+	courseItem := &course_management.CourseItem{
+		CourseId:          course.CourseId,
+		CourseName:        course.CourseName,
+		CourseDescription: course.CourseDescription,
+		FacultyDepartment: course.FacultyDepartment,
+		AcademicTerm:      course.AcademicTerm,
+		AcademicYear:      course.AcademicYear,
 		Professors:        []*course_management.Professor{},
 		Prerequisites:     []*course_management.Prerequisite{},
-		Status:            subject.Status,
-		CurriculumName:    subject.CurriculumName,
-		DegreeLevel:       subject.DegreeLevel,
-		TeachingHours:     subject.TeachingHours,
-	}, nil
-}
-
-func (s *Server) AddNewSubject(ctx context.Context, req *course_management.CourseItem) (*course_management.CourseItem, error) {
-	log.Println("AddNewSubject", req.GetCourseId())
-	subject := models.Course{
-		CourseId:          req.GetCourseId(),
-		CourseName:        req.GetCourseName(),
-		CourseDescription: req.GetCourseDescription(),
-		FacultyDepartment: req.GetFacultyDepartment(),
-		AcademicTerm:      req.GetAcademicTerm(),
-		AcademicYear:      req.GetAcademicYear(),
-		Professors:        "test",
-		Prerequisites:     "test",
-		Status:            req.GetStatus(),
-		CurriculumName:    req.GetCurriculumName(),
-		DegreeLevel:       req.GetDegreeLevel(),
-		TeachingHours:     req.GetTeachingHours(),
+		Status:            course.Status,
+		CurriculumName:    course.CurriculumName,
+		DegreeLevel:       course.DegreeLevel,
+		TeachingHours:     course.TeachingHours,
 	}
-	db.Create(&subject)
+	// Return
+	return courseItem, nil
+}
+
+func (s *Server) AddNewCourse(ctx context.Context, req *course_management.CourseItem) (*course_management.CourseItem, error) {
+	log.Print("AddNewCourse")
+	course := models.NewCourse(
+		req.GetCourseId(),
+		req.GetCourseName(),
+		req.GetCourseDescription(),
+		req.GetFacultyDepartment(),
+		req.GetAcademicTerm(),
+		req.GetAcademicYear(),
+		"test",
+		"test",
+		req.GetStatus(),
+		req.GetCurriculumName(),
+		req.GetDegreeLevel(),
+		req.GetTeachingHours(),
+	)
+	// Add new course to database
+	db.Create(&course)
+	// Return
 	return &course_management.CourseItem{
-		CourseId:          req.GetCourseId(),
-		CourseName:        req.GetCourseName(),
-		CourseDescription: req.GetCourseDescription(),
-		FacultyDepartment: req.GetFacultyDepartment(),
-		AcademicTerm:      req.GetAcademicTerm(),
-		AcademicYear:      req.GetAcademicYear(),
-		Professors:        req.GetProfessors(),
-		Prerequisites:     req.GetPrerequisites(),
-		Status:            req.GetStatus(),
-		CurriculumName:    req.GetCurriculumName(),
-		DegreeLevel:       req.GetDegreeLevel(),
-		TeachingHours:     req.GetTeachingHours(),
+		CourseId:          course.CourseId,
+		CourseName:        course.CourseName,
+		CourseDescription: course.CourseDescription,
+		FacultyDepartment: course.FacultyDepartment,
+		AcademicTerm:      course.AcademicTerm,
+		AcademicYear:      course.AcademicYear,
+		Professors:        []*course_management.Professor{},
+		Prerequisites:     []*course_management.Prerequisite{},
+		Status:            course.Status,
+		CurriculumName:    course.CurriculumName,
+		DegreeLevel:       course.DegreeLevel,
+		TeachingHours:     course.TeachingHours,
 	}, nil
 }
 
-func (s *Server) UpdateSubjectDetail(ctx context.Context, req *course_management.CourseItem) (*course_management.CourseItem, error) {
-	log.Println("UpdateSubjectDetail")
-	log.Println("Updating ", req.String())
-	subject := models.Course{
-		CourseId:          req.GetCourseId(),
-		CourseName:        req.GetCourseName(),
-		CourseDescription: req.GetCourseDescription(),
-		FacultyDepartment: req.GetFacultyDepartment(),
-		AcademicTerm:      req.GetAcademicTerm(),
-		AcademicYear:      req.GetAcademicYear(),
-		Professors:        "test",
-		Prerequisites:     "test",
-		Status:            req.GetStatus(),
-		CurriculumName:    req.GetCurriculumName(),
-		DegreeLevel:       req.GetDegreeLevel(),
-		TeachingHours:     req.GetTeachingHours(),
-	}
-	db.Save(&subject)
+func (s *Server) UpdateCourseDetail(ctx context.Context, req *course_management.CourseItem) (*course_management.CourseItem, error) {
+	log.Println("UpdateCourseDetail")
+	// Get course in database
+	var course models.Course
+	db.First(&course, req.GetCourseId())
+	// Update course
+	course.CourseName = req.GetCourseName()
+	course.CourseDescription = req.GetCourseDescription()
+	course.FacultyDepartment = req.GetFacultyDepartment()
+	course.AcademicTerm = req.GetAcademicTerm()
+	course.AcademicYear = req.GetAcademicYear()
+	course.Status = req.GetStatus()
+	course.CurriculumName = req.GetCurriculumName()
+	course.DegreeLevel = req.GetDegreeLevel()
+	course.TeachingHours = req.GetTeachingHours()
+	db.Save(&course)
+	// Return
 	return &course_management.CourseItem{
-		CourseName:        req.GetCourseId(),
-		CourseId:          req.GetCourseName(),
-		CourseDescription: req.GetCourseDescription(),
-		FacultyDepartment: req.GetFacultyDepartment(),
-		AcademicTerm:      req.GetAcademicTerm(),
-		AcademicYear:      req.GetAcademicYear(),
-		Professors:        req.GetProfessors(),
-		Prerequisites:     req.GetPrerequisites(),
-		Status:            req.GetStatus(),
-		CurriculumName:    req.GetCurriculumName(),
-		DegreeLevel:       req.GetDegreeLevel(),
-		TeachingHours:     req.GetTeachingHours(),
+		CourseId:          course.CourseId,
+		CourseName:        course.CourseName,
+		CourseDescription: course.CourseDescription,
+		FacultyDepartment: course.FacultyDepartment,
+		AcademicTerm:      course.AcademicTerm,
+		AcademicYear:      course.AcademicYear,
+		Professors:        []*course_management.Professor{},
+		Prerequisites:     []*course_management.Prerequisite{},
+		Status:            course.Status,
+		CurriculumName:    course.CurriculumName,
+		DegreeLevel:       course.DegreeLevel,
+		TeachingHours:     course.TeachingHours,
 	}, nil
 }
 
-func (s *Server) DeleteSubject(ctx context.Context, req *course_management.CourseId) (*course_management.Empty, error) {
-	log.Println("DeleteSubject")
-	log.Println("Deleting ", req.GetCourseId())
-	db.Delete(&models.Course{}, req.GetCourseId())
+func (s *Server) DeleteCourse(ctx context.Context, req *course_management.CourseId) (*course_management.Empty, error) {
+	log.Println("DeleteCourse", req.GetCourseId())
+	// Get course in database
+	var course models.Course
+	db.First(&course, req.GetCourseId())
+	// Delete course
+	db.Delete(&course)
+	// Return
 	return &course_management.Empty{}, nil
 }
