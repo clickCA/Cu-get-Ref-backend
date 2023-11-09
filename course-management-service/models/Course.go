7be5d806737824a819ff84fbc8e602fd4db18dcb
Nick <nick.rattanaton@gmail.com>
2023-11-09 21:38:16 +0700

    ➗ Update with constrain

--- course-management-service/models/Course.go
+++ course-management-service/models/Course.go
@@ -7,27 +7,20 @@ import (
 
 type Course struct {
 	CourseId          string `gorm:"primaryKey"`
-	CourseName        string
-	CourseDescription string
-	FacultyDepartment string
-	AcademicTerm      string
-	AcademicYear      int32
-	Professors        []Professor    `gorm:"many2many:course_professors;"`
-	Prerequisites     []Prerequisite `gorm:"many2many:course_prerequisites;"`
-	Status            string
-	CurriculumName    string
-	DegreeLevel       string
-	TeachingHours     int32
-}
-type Professor struct {
-	ProfessorName string `gorm:"primaryKey"`
+	CourseName        string `gorm:"type:varchar(50)"`
+	CourseDescription string `gorm:"type:text"`
+	FacultyDepartment string `gorm:"type:varchar(50)"`
+	AcademicTerm      string `gorm:"type:varchar(10)"`
+	AcademicYear      int32  `gorm:"type:smallint"`
+	Professors        string `gorm:"type:text"`
+	Prerequisites     string `gorm:"type:text"`
+	Status            string `gorm:"type:varchar(10)"`
+	CurriculumName    string `gorm:"type:varchar(50)"`
+	DegreeLevel       string `gorm:"type:varchar(10)"`
+	TeachingHours     int32  `gorm:"type:smallint"`
 }
 
-type Prerequisite struct {
-	PrerequisiteId string `gorm:"primaryKey"`
-}
-
-func NewCourse(courseId string, courseName string, courseDescription string, facultyDepartment string, academicTerm string, academicYear int32, professors []Professor, prerequisites []Prerequisite, status string, curriculumName string, degreeLevel string, teachingHours int32) (*Course, error) {
+func NewCourse(courseId string, courseName string, courseDescription string, facultyDepartment string, academicTerm string, academicYear int32, professors string, prerequisites string, status string, curriculumName string, degreeLevel string, teachingHours int32) (*Course, error) {
 	match, _ := regexp.MatchString(`\d{7}`, courseId)
 	if !match {
 		return nil, errors.New("courseId must be 7 digits")
@@ -75,4 +68,53 @@ func NewCourse(courseId string, courseName string, courseDescription string, fac
 	course.DegreeLevel = degreeLevel
 	course.TeachingHours = teachingHours
 	return course, nil
+}
+
+func UpdateCourse(oldCourse *Course, courseId string, courseName string, courseDescription string, facultyDepartment string, academicTerm string, academicYear int32, professors string, prerequisites string, status string, curriculumName string, degreeLevel string, teachingHours int32) error {
+	match, _ := regexp.MatchString(`\d{7}`, courseId)
+	if !match {
+		return errors.New("courseId cannot be empty")
+	}
+	if courseName == "" {
+		return errors.New("courseName cannot be empty")
+	}
+	if courseDescription == "" {
+		return errors.New("courseDescription cannot be empty")
+	}
+	if facultyDepartment == "" {
+		return errors.New("facultyDepartment cannot be empty")
+	}
+	if academicTerm == "" {
+		return errors.New("academicTerm cannot be empty")
+	}
+	if academicYear == 0 {
+		return errors.New("academicYear cannot be empty")
+	}
+	match, _ = regexp.MatchString(`(open)|(close)`, status)
+	if !match {
+		return errors.New("status must be open or close")
+	}
+	if curriculumName == "" {
+		return errors.New("curriculumName cannot be empty")
+	}
+	match, _ = regexp.MatchString(`(bachelor)|(master)|(doctoral)`, degreeLevel)
+	if !match {
+		return errors.New("degreeLevel must be bachelor, master or doctoral")
+	}
+	if teachingHours <= 0 {
+		return errors.New("teachingHours must be greater than 0")
+	}
+	oldCourse.CourseId = courseId
+	oldCourse.CourseName = courseName
+	oldCourse.CourseDescription = courseDescription
+	oldCourse.FacultyDepartment = facultyDepartment
+	oldCourse.AcademicTerm = academicTerm
+	oldCourse.AcademicYear = academicYear
+	oldCourse.Professors = professors
+	oldCourse.Prerequisites = prerequisites
+	oldCourse.Status = status
+	oldCourse.CurriculumName = curriculumName
+	oldCourse.DegreeLevel = degreeLevel
+	oldCourse.TeachingHours = teachingHours
+	return nil
 }
--- course-management-service/services/coursemanagement_services.go
+++ course-management-service/services/coursemanagement_services.go
@@ -6,6 +6,7 @@ import (
 	course_management "course-management-service/coursemanagement"
 	"course-management-service/models"
 	"log"
+	"strings"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -39,8 +40,8 @@ func (s *Server) GetAllCourses(ctx context.Context, req *course_management.Empty
 			FacultyDepartment: subject.FacultyDepartment,
 			AcademicTerm:      subject.AcademicTerm,
 			AcademicYear:      subject.AcademicYear,
-			Professors:        []*course_management.Professor{},
-			Prerequisites:     []*course_management.Prerequisite{},
+			Professors:        convertStringToProfessors(subject.Professors),
+			Prerequisites:     convertStringToPrerequisites(subject.Prerequisites),
 			Status:            subject.Status,
 			CurriculumName:    subject.CurriculumName,
 			DegreeLevel:       subject.DegreeLevel,
@@ -74,8 +75,8 @@ func (s *Server) GetCourse(ctx context.Context, req *course_management.CourseId)
 		FacultyDepartment: course.FacultyDepartment,
 		AcademicTerm:      course.AcademicTerm,
 		AcademicYear:      course.AcademicYear,
-		Professors:        []*course_management.Professor{},
-		Prerequisites:     []*course_management.Prerequisite{},
+		Professors:        convertStringToProfessors(course.Professors),
+		Prerequisites:     convertStringToPrerequisites(course.Prerequisites),
 		Status:            course.Status,
 		CurriculumName:    course.CurriculumName,
 		DegreeLevel:       course.DegreeLevel,
@@ -87,21 +88,9 @@ func (s *Server) GetCourse(ctx context.Context, req *course_management.CourseId)
 
 func (s *Server) AddNewCourse(ctx context.Context, req *course_management.CourseItem) (*course_management.CourseItem, error) {
 	log.Print("AddNewCourse")
+	professors_string := convertProfessorsToString(req.GetProfessors())
+	prerequisites_string := convertPrerequisitesToString(req.GetPrerequisites())
 	// Create new course
-	// Map professors
-	var professors []models.Professor
-	for _, professor := range req.GetProfessors() {
-		professors = append(professors, models.Professor{
-			ProfessorName: professor.GetProfessorName(),
-		})
-	}
-	// Map prerequisites
-	var prerequisites []models.Prerequisite
-	for _, prerequisite := range req.GetPrerequisites() {
-		prerequisites = append(prerequisites, models.Prerequisite{
-			PrerequisiteId: prerequisite.GetCourseId(),
-		})
-	}
 	course, err := models.NewCourse(
 		req.GetCourseId(),
 		req.GetCourseName(),
@@ -109,8 +98,8 @@ func (s *Server) AddNewCourse(ctx context.Context, req *course_management.Course
 		req.GetFacultyDepartment(),
 		req.GetAcademicTerm(),
 		req.GetAcademicYear(),
-		professors,
-		prerequisites,
+		professors_string,
+		prerequisites_string,
 		req.GetStatus(),
 		req.GetCurriculumName(),
 		req.GetDegreeLevel(),
@@ -163,16 +152,33 @@ func (s *Server) UpdateCourseDetail(ctx context.Context, req *course_management.
 		)
 		return nil, err
 	}
+	// Map to string
+	professors_string := convertProfessorsToString(req.GetProfessors())
+	prerequisites_string := convertPrerequisitesToString(req.GetPrerequisites())
+	// Create new course
+	err := models.UpdateCourse(
+		&course,
+		req.GetCourseId(),
+		req.GetCourseName(),
+		req.GetCourseDescription(),
+		req.GetFacultyDepartment(),
+		req.GetAcademicTerm(),
+		req.GetAcademicYear(),
+		professors_string,
+		prerequisites_string,
+		req.GetStatus(),
+		req.GetCurriculumName(),
+		req.GetDegreeLevel(),
+		req.GetTeachingHours(),
+	)
+	if err != nil {
+		err = status.Errorf(
+			codes.InvalidArgument,
+			err.Error(),
+		)
+		return nil, err
+	}
 	// Update course
-	course.CourseName = req.GetCourseName()
-	course.CourseDescription = req.GetCourseDescription()
-	course.FacultyDepartment = req.GetFacultyDepartment()
-	course.AcademicTerm = req.GetAcademicTerm()
-	course.AcademicYear = req.GetAcademicYear()
-	course.Status = req.GetStatus()
-	course.CurriculumName = req.GetCurriculumName()
-	course.DegreeLevel = req.GetDegreeLevel()
-	course.TeachingHours = req.GetTeachingHours()
 	result = db.Save(&course)
 	if result.Error != nil {
 		err := status.Errorf(
@@ -190,8 +196,8 @@ func (s *Server) UpdateCourseDetail(ctx context.Context, req *course_management.
 		FacultyDepartment: course.FacultyDepartment,
 		AcademicTerm:      course.AcademicTerm,
 		AcademicYear:      course.AcademicYear,
-		Professors:        []*course_management.Professor{},
-		Prerequisites:     []*course_management.Prerequisite{},
+		Professors:        convertStringToProfessors(course.Professors),
+		Prerequisites:     convertStringToPrerequisites(course.Prerequisites),
 		Status:            course.Status,
 		CurriculumName:    course.CurriculumName,
 		DegreeLevel:       course.DegreeLevel,
@@ -224,4 +230,60 @@ func (s *Server) DeleteCourse(ctx context.Context, req *course_management.Course
 	}
 	// Return
 	return &course_management.Empty{}, nil
+}
+
+func convertProfessorsToString(professors []*course_management.Professor) string {
+	var professors_string string
+	for _, professor := range professors {
+		// if the last element skip ||
+		if professor == professors[len(professors)-1] {
+			professors_string += professor.GetProfessorName()
+			continue
+		}
+		professors_string += professor.GetProfessorName() + "||"
+	}
+	return professors_string
+}
+
+func convertPrerequisitesToString(prerequisites []*course_management.Prerequisite) string {
+	var prerequisites_string string
+	for _, prerequisite := range prerequisites {
+		// if the last element skip ||
+		if prerequisite == prerequisites[len(prerequisites)-1] {
+			prerequisites_string += prerequisite.GetCourseId()
+			continue
+		}
+		prerequisites_string += prerequisite.GetCourseId() + "||"
+	}
+	return prerequisites_string
+}
+
+func convertStringToProfessors(professors string) []*course_management.Professor {
+	var professors_list []*course_management.Professor
+	professors_array := strings.Split(professors, "||")
+	for _, professor := range professors_array {
+		// Skip empty string
+		if professor == "" {
+			continue
+		}
+		professors_list = append(professors_list, &course_management.Professor{
+			ProfessorName: professor,
+		})
+	}
+	return professors_list
+}
+
+func convertStringToPrerequisites(prerequisites string) []*course_management.Prerequisite {
+	var prerequisites_list []*course_management.Prerequisite
+	prerequisites_array := strings.Split(prerequisites, "||")
+	for _, prerequisite := range prerequisites_array {
+		// Skip empty string
+		if prerequisite == "" {
+			continue
+		}
+		prerequisites_list = append(prerequisites_list, &course_management.Prerequisite{
+			CourseId: prerequisite,
+		})
+	}
+	return prerequisites_list
 }
